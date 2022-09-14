1e63661f334583c220ff9f33370a0c297ac9ef55
elbaley <lebafurkan@outlook.com>
2022-09-14 22:26:02 +0300

    add persistent state w async storage

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native'
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import Header from './src/components/Header'
 import List from './src/components/List'
 import TaskInput from './src/components/TaskInput'
 
 const App = () => {
   const [tasks, setTasks] = useState([
-    {id: 0, text: 'Lorem ipsum dolor sit amet.', completed: true},
+    // {id: 0, text: 'Lorem ipsum dolor sit amet.', completed: true},
   ])
+
+  const storeData = async value => {
+    const stringifiedState = JSON.stringify(value)
+    try {
+      console.log('saving state on the phone...')
+      await AsyncStorage.setItem('localTasks', stringifiedState)
+    } catch (e) {
+      // saving error
+    }
+  }
+
+  // const getData = async () => {
+  //   try {
+  //     const value = await AsyncStorage.getItem('localTasks')
+  //     if (value !== null) {
+  //       // value previously stored
+  //       return value
+  //     }
+  //   } catch (e) {
+  //     // error reading value
+  //   }
+  // }
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('localTasks')
+      if (jsonValue !== null) {
+        console.log('fonksiyondan geliyorum', JSON.parse(jsonValue))
+        return JSON.parse(jsonValue)
+      }
+    } catch (e) {
+      // error reading value
+    }
+  }
+  useEffect(() => {
+    if (tasks.length === 0) {
+      //get tasks from local storage
+      getData()
+        .then(res => {
+          console.log('useEFFECTTEN', res)
+          setTasks(res)
+        })
+        .catch(err => {
+          console.log('ERRRORR')
+        })
+    } else {
+      storeData(tasks)
+    }
+  }, [tasks])
+
   function addTask(text) {
     const taskObj = {
       id: tasks.length,
